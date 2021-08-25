2a530cbfb6508995338142215612bb7005e0ab1d
ZarinaMambetova <zarina.mambetova84@gmail.com>
2021-08-25 22:18:12 +0300

    Обработка кликов

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="board__wrapper">
     <div class="board">
-      <BoardItem v-for="field in fields" :field="field" :key="'item-' + field.id"/>
+      <BoardItem :game-Status="gameStatus" v-for="field in fields" :field="field" :key="'item-' + field.id" 
+      @selectField="selectField($event)"/>
     </div>
     <p class="difficult">Сложность: <strong>{{difficult}}</strong></p>
-    <button class="btn" @click="start">Старт</button>
+    <button class="btn" @click="start" :disabled="!canStartGame">Старт</button>
   </div>
 </template>
 
 <script>
   import BoardItem from './BoardItem';
-  import { onBeforeMount, ref } from 'vue';
+  import { onBeforeMount, ref, computed } from 'vue';
+  import { FIELD, DEFAULT_DIFFICULT, GAME_STATUS, GAME_SPEED, MAX_DIFFICULT } from '@/constants.js';
 
   export default {
     name: 'Board',
@@ -19,10 +21,58 @@
       BoardItem, 
       },
       setup() {
-        let difficult = ref(3);
+
+        let difficult = ref(DEFAULT_DIFFICULT);
         let fields = ref([]);
         const number = 25;
+       const gameStatus = ref(GAME_STATUS.NONE);
+       const selectField = (id) => {
+         const index = fields.value.findIndex((field) => {
+           return field.id === id;
+           
+         });
+         if (index > -1 && !fields.value[index].clicked) {
+           fields.value[index].clicked = true;
 
+           checkGame();
+
+         }
+       };
+        const checkGame = () => {
+            const errorIndex = fields.value.findIndex(field => {
+              return field.clicked && field.value === FIELD.EMPTY;
+            });
+            if (errorIndex > -1) {
+              setGameOver();
+              return;
+            }
+            const notFoundItemIndex = fields.value.findIndex(field => {
+              return !field.clicked && field.value === FIELD.FILLED;
+            });
+
+            if (notFoundItemIndex === -1) {
+                setWin();
+            }
+        }
+
+        const setGameOver = () => {
+          console.log('fail');
+            gameStatus.value = GAME_STATUS.FAIL;
+            difficult.value = DEFAULT_DIFFICULT;
+        }
+
+        const setWin = () => {
+          if (difficult.value < MAX_DIFFICULT) {
+             difficult.value += 1;
+           
+          } else {
+              difficult.value = DEFAULT_DIFFICULT;
+          }
+           
+
+        }
+
+        
         const init = () => {
           fields.value = [];
           fields.value = [];
@@ -31,11 +81,16 @@
             fields.value.push({
               id: i,
               clicked: false,
-              value: 0,
+              value: FIELD.EMPTY,
             });
           }
 
         }
+
+      const canStartGame = computed(() => {
+          return gameStatus.value !== GAME_STATUS.PREVIEW;
+
+        });
 
         onBeforeMount(init);
 
@@ -43,29 +98,46 @@
           number,
           difficult,
           fields,
-          init
+          init, 
+          gameStatus,
+          canStartGame,
+          selectField,
+          checkGame
         }
+        
       },
+
       methods: {
+
         start() {
           this.init();
           this.prepareGame();
         },
         prepareGame() {
+          this.gameStatus = GAME_STATUS.PREVIEW;
+          
+
           for (let i = 0; i < this.difficult; i++) {
             const index = this.rand(0, this.number -1);  
 
-            if (this.fields[index].value !==1) {
-              this.fields[index].value = 1;
+            if (this.fields[index].value !==FIELD.FILLED) {
+              this.fields[index].value = FIELD.FILLED;
             } else {
               i--;
             }
           }
+
+          setTimeout(() => {
+            this.gameStatus = GAME_STATUS.STARTED;
+          }, GAME_SPEED);
         },
         rand(min, max) {
           return Math.floor(Math.random() * (max - min)) + min;
         },
-      }
+
+
+      },
+
   }
 </script>
 
@@ -94,5 +166,9 @@
 
   .btn:hover {
     background: #42b983;
+  }
+
+  button:disabled {
+    opacity: .5;
   }
 </style>
--- src/components/BoardItem.vue
+++ src/components/BoardItem.vue
@@ -1,17 +1,49 @@
 <template>
-  <span :class="'item ' + (field.value === 1 ? 'item-active' : '')"></span>
+  <span :class="getBoardItemClasses" @click="select(field.id)"></span>
 </template>
 
 <script>
+import { GAME_STATUS, FIELD } from '@/constants.js';
+import { computed } from 'vue';
+
 export default {
   name: 'BoardItem',
   props: {
     field: {
       type: Object,
       required: true,
+    },
+    gameStatus: {
+      type: Number,
+      required: false,
+      default: GAME_STATUS.NONE,
+    }
+  },
+  setup(props) {
+  const getBoardItemClasses = computed(() => {
+let classes = 'item ';
+
+if (props.field.value === FIELD.FILLED && props.gameStatus === GAME_STATUS.PREVIEW 
+|| props.field.clicked && props.field.value === FIELD.FILLED) {
+  classes += 'active';
+}
+if (props.field.clicked && props.field.value === FIELD.EMPTY) {
+  classes += ' error';
+}
+return classes;
+  });
+  return { getBoardItemClasses }
+},
+methods: {
+  select(id) {
+    if (this.gameStatus === GAME_STATUS.STARTED) {
+          this.$emit('selectField', id);
     }
   }
 }
+}
+
+
 
 </script>
 
@@ -28,9 +60,12 @@ export default {
     transform-style: preserve-3d;
   }
 
-  .item-active {
+  .item.active {
     background: #42b983cc;
     transform: rotateX(180deg);
   }
-
+  .item.error {
+    background: #ff000055;
+    transform: rotateX(180deg);
+  }
 </style>
